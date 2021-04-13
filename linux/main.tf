5f3fed1e294245d0d82c01d30df11735e9f3fac5
Chris Mays <cmays@outlook.com>
2021-04-13 08:30:30 -0500

    Create main.tf

---
+++ linux/main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-east-2"
+}
+
+# Default security group to access the instances via WinRM over HTTP and HTTPS
+resource "aws_security_group" "linux-httpd" {
+  name        = "linux-httpd"
+  description = "Used in the terraform"
+
+  # WinRM access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "linux_httpd" {
+  ami = "ami-03d64741867e7bb94"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "RHEL-HTTPD"
+    owner = "cmays"
+    AOC_DEMO_ROLE = "httpd"
+    DemoID = "cmays"
+  }
+  key_name = "deployer-key-linux"
+
+  # Our Security group to allow WinRM access
+  vpc_security_group_ids = [aws_security_group.linux-httpd.id]
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name = "deployer-key-linux"
+  public_key = var.public_key
+}
