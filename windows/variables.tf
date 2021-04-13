145a082866d233a8d327023b252593a08a9863f0
Chris Mays <cmays@outlook.com>
2021-04-13 13:42:22 -0500

    Create variables.tf

---
+++ windows/variables.tf
@@ -0,0 +1,14 @@
+variable "public_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "win_username" {
+  type      = string
+  sensitive = true
+}
+
+variable "win_password" {
+  type      = string
+  sensitive = true
+}
